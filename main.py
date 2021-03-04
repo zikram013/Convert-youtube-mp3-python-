c09837ff1bae34e220e6fd48fabca1bcf6b9b24e
zikram013 <32304189+zikram013@users.noreply.github.com>
2021-03-04 17:09:37 +0100

    arreglo de bugs e instalacion del paquete en pycharm

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ tituloVideo = video["title"]
 
 opciones = {
     "format": "bestaudio/best",
-    "outtmpl": f"E:/",
+    "outtmpl": f"E:/{tituloVideo}.mp3",
     "postprocesosors": [{
         "key": "FFmpegExtractAudio",
         "preferredcodec": "mp3",
@@ -16,4 +16,4 @@ opciones = {
 }
 
 with youtube_dl.YoutubeDL(opciones)as ydl:
-    ydl.dowonload([enlace])
+    ydl.download([enlace])
