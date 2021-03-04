640b6b482558c2d5efd502c15bf698887163a4e8
zikram013 <32304189+zikram013@users.noreply.github.com>
2021-03-04 17:04:33 +0100

    code descarga

---
+++ main.py
@@ -0,0 +1,19 @@
+import youtube_dl
+
+enlace = input("introducir Url del video")
+
+video = youtube_dl.YoutubeDL().extract_info(url=enlace, download=False)
+tituloVideo = video["title"]
+
+opciones = {
+    "format": "bestaudio/best",
+    "outtmpl": f"E:/",
+    "postprocesosors": [{
+        "key": "FFmpegExtractAudio",
+        "preferredcodec": "mp3",
+        "preferredquality": "192"
+    }],
+}
+
+with youtube_dl.YoutubeDL(opciones)as ydl:
+    ydl.dowonload([enlace])
